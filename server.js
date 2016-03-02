b01907c2f01c69cfbdfbe66dfb400ff8d159c13a
Breiden Busch <breiden.busch@gmail.com>
2016-03-02 16:03:49 -0700

    Finished Through Step 4

--- server.js
+++ server.js
@@ -1,1 +1,57 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
+
+var messages = [{
+  time: new Date(),
+  message: 'Hello!'
+}];
+
+
+
+app.get('/', function(req, res) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).json(messages);
+});
+
+app.options('/', function(req, res) {
+  res.status(200).set({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+  }).json(messages);
+});
+
+app.post('/', function(req, res) {
+  messages.push({
+    message: req.body.message,
+    time: new Date()
+  });
+
+  res.status(200).set({
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+  'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+  'X-XSS-Protection': '1; mode=block',
+  'X-Frame-Options': 'SAMEORIGIN',
+  'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+}).json(messages);
+
+});
+
+
+
+
+app.listen(9000, function() {
+  console.log('All Clear, Captain!');
+});
